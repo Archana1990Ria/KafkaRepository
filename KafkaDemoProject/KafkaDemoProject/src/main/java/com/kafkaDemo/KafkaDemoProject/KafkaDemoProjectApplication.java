5e2d485fc5195c040a50a91a566a888024899ffe
Archana1990Ria <56785013+Archana1990Ria@users.noreply.github.com>
2024-10-26 12:51:32 -0500

    Add files via upload

---
+++ KafkaDemoProject/KafkaDemoProject/src/main/java/com/kafkaDemo/KafkaDemoProject/KafkaDemoProjectApplication.java
@@ -0,0 +1,16 @@
+package com.kafkaDemo.KafkaDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KafkaDemoProjectApplication.class, args);
+		System.out.println("Hi Archana");
+
+	}
+
+}
